136c2f14e5f476939fab3454e14ad6a900ebc118
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-11-14 17:42:54 -0700

    language support

--- web/nature/language/localize.js
+++ web/nature/language/localize.js
@@ -2,10 +2,26 @@ const localization_maps = [
     "language/base.json"
 ];
 
-function localize(source, language){
+function localize_from_source(source, language){
     fetch(source).then(res => res.json()).then(res => {
+        // inner html
         for(const [id, value] of Object.entries(res[language])){
-            document.getElementById(id).innerHTML = value;
+            for(const element of document.querySelectorAll(`[langtag='${id}']`)){
+                element.innerHTML = value;
+            }
+        }
+
+        // placeholders
+        for(const [id, value] of Object.entries(res[language])){
+            for(const element of document.querySelectorAll(`[langplaceholder='${id}']`)){
+                element.placeholder = value;
+            }
         }
     }).catch(err => console.error(`Unable to load language translation source [${source}] because [${err}]`));
+}
+
+function localize(language){
+    for(const source of localization_maps){
+        localize_from_source(source, language);
+    }
 }
